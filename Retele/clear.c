17fd963fbb52b28a171d1ad261fda5953c70caa7
alextudorpufi <153077237+alextudorpufi@users.noreply.github.com>
2024-10-23 21:51:44 +0300

    Delete Retele directory

--- Retele/clear.c
+++
@@ -1,9 +0,0 @@
-#define client_to_server_fifo "client_to_server_fifo"
-#define server_to_client_fifo "server_to_client_fifo"
-
-int main()
-{
-    unlink(client_to_server_fifo);
-    unlink(server_to_client_fifo);
-    return 0;
-}
